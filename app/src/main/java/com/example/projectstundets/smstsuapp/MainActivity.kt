0caa1d2bef0856ccac8442c2b290c27fc422236d
Saba-21 <stoningrolle@gmail.com>
2018-04-26 02:20:17 +0400

    splash screen

--- app/src/main/java/com/example/projectstundets/smstsuapp/MainActivity.kt
+++ app/src/main/java/com/example/projectstundets/smstsuapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.projectstundets.smstsuapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,14 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fragmentManager.beginTransaction().replace(R.id.screen_main_placeholder, SplashFragment.newInstance()).commit()
-
-        val handler = Handler()
-        handler.post {
-            handler.postDelayed({endSplashScreen()},5000)
-        }
-    }
-    private fun endSplashScreen(){
         fragmentManager.beginTransaction().replace(R.id.screen_main_placeholder, MainFragment.newInstance()).commit()
+
     }
 }
